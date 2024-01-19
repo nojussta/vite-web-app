1dbf19ea0d269c6fd2086648f6923975615802f4
nojussta <nojus.stasiunas2@gmail.com>
2024-01-20 01:38:10 +0200

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import ReactLogo from './assets/react.svg';
+import ViteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const environment = process.env.ENVIRONMENT || 'dev';
+  const version = `v1.${environment}`;
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={ViteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={ReactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -31,8 +33,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className="version">Version: {version}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
