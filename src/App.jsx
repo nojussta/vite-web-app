6d7a23b080d0dc3a5d7cd890675fb94d202a664f
nojussta <nojus.stasiunas2@gmail.com>
2024-01-20 00:12:09 +0200

    Text for testing

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>
+          Additional text for testing purposes
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
